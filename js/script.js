aade878e9091bc10a0f83f3c452f5bb86428d22e
rohit kumar <rohit1071998@gmail.com>
2020-06-11 16:59:32 +0530

    Added Search functionality

--- js/script.js
+++ js/script.js
@@ -3,16 +3,24 @@ function fetch() {
     $.get('https://api.covid19api.com/summary', function(data){
 
     // for world covid cases
-    var wrow = $("<tr>");
 
-    wrow.append($('<td>'+ data.Global.TotalConfirmed +' </td>'))
-       .append($('<td>'+ data.Global.TotalRecovered +' </td>'))
-       .append($('<td>'+ data.Global.TotalDeaths +' </td>'))
-       .append($('<td>'+ data.Global.NewConfirmed +' </td>'))
-       .append($('<td>'+ data.Global.NewRecovered +' </td>'))
-       .append($('<td>'+ data.Global.NewDeaths +' </td>'))
+    $('.world-TotalConfirmed').text(data.Global.TotalConfirmed);
+    $('.world-TotalRecovered').text(data.Global.TotalRecovered);
+                $('.world-TotalDeaths').text(data.Global.TotalDeaths);
+                $('.world-NewConfirmed').text(data.Global.NewConfirmed);
+                $('.world-NewRecovered').text(data.Global.NewRecovered);
+                $('.world-NewDeaths').text(data.Global.NewDeaths);
+
+    // var wrow = $("<tr>");
+
+    // wrow.append($('<td>'+ data.Global.TotalConfirmed +' </td>'))
+    //    .append($('<td>'+ data.Global.TotalRecovered +' </td>'))
+    //    .append($('<td>'+ data.Global.TotalDeaths +' </td>'))
+    //    .append($('<td>'+ data.Global.NewConfirmed +' </td>'))
+    //    .append($('<td>'+ data.Global.NewRecovered +' </td>'))
+    //    .append($('<td>'+ data.Global.NewDeaths +' </td>'))
    
-    $("#world_tbval").append(wrow);
+    // $("#world_tbval").append(wrow);
 
 
         
@@ -20,16 +28,23 @@ function fetch() {
         {
             if(data.Countries[i].Country === "India") {
 
-                var irow = $("<tr>");
+                $('.india-TotalConfirmed').text(data.Countries[i].TotalConfirmed);
+                $('.india-TotalRecovered').text(data.Countries[i].TotalRecovered);
+                $('.india-TotalDeaths').text(data.Countries[i].TotalDeaths);
+                $('.india-NewConfirmed').text(data.Countries[i].NewConfirmed);
+                $('.india-NewRecovered').text(data.Countries[i].NewRecovered);
+                $('.india-NewDeaths').text(data.Countries[i].NewDeaths);
 
-            irow.append($('<td>'+ data.Countries[i].TotalConfirmed +' </td>'))
-               .append($('<td>'+ data.Countries[i].TotalRecovered +' </td>'))
-               .append($('<td>'+ data.Countries[i].TotalDeaths +' </td>'))
-               .append($('<td>'+ data.Countries[i].NewConfirmed +' </td>'))
-               .append($('<td>'+ data.Countries[i].NewRecovered +' </td>'))
-               .append($('<td>'+ data.Countries[i].NewDeaths +' </td>'))
+            //     var irow = $("<tr>");
+
+            // irow.append($('<td>'+ data.Countries[i].TotalConfirmed +' </td>'))
+            //    .append($('<td>'+ data.Countries[i].TotalRecovered +' </td>'))
+            //    .append($('<td>'+ data.Countries[i].TotalDeaths +' </td>'))
+            //    .append($('<td>'+ data.Countries[i].NewConfirmed +' </td>'))
+            //    .append($('<td>'+ data.Countries[i].NewRecovered +' </td>'))
+            //    .append($('<td>'+ data.Countries[i].NewDeaths +' </td>'))
            
-            $("#india_tbval").append(irow);
+            // $("#india_tbval").append(irow);
 
             }
 
@@ -47,3 +62,50 @@ function fetch() {
         }
     });
 }
+
+
+$('button').click(displayCountry);
+// Search by Enter Key
+$('input').on("keyup", function(event){
+    if(event.keyCode == 13 && $('input').val() != "") {
+        displayCountry();
+    }
+});
+
+function displayCountry(){
+    var countryName = $('input').val();
+
+    if(countryName == "") {
+        alert("Please Enter Country Name !");
+    }
+    countryName = countryName[0].toUpperCase() + countryName.slice(1);
+
+    $.get('https://api.covid19api.com/summary', function(data){
+        let i = 0;
+        for(; i < data.Countries.length; i++)
+        {
+            if(data.Countries[i].Country == countryName) {
+
+                console.log(countryName);
+                $('#serch-contry').css("display","block");
+                $('#contry-name').text(data.Countries[i].Country);
+
+                $('.search-TotalConfirmed').text(data.Countries[i].TotalConfirmed);
+                $('.search-TotalRecovered').text(data.Countries[i].TotalRecovered);
+                $('.search-TotalDeaths').text(data.Countries[i].TotalDeaths);
+                $('.search-NewConfirmed').text(data.Countries[i].NewConfirmed);
+                $('.search-NewRecovered').text(data.Countries[i].NewRecovered);
+                $('.search-NewDeaths').text(data.Countries[i].NewDeaths);
+                break;
+            }
+           
+        }
+
+        if(i == data.Countries.length) {
+            let display = countryName + " Country not founded!"
+            alert(display);
+        }
+        
+    });
+    
+};
